9d0b0bf7678058ad3fd3222916c75490e9e10682
AminRezaei-68 <rezaei.amin@gmail.com>
2025-02-26 15:05:10 +0330

    remove some comment code

--- C-invoice/src/d-svc/d-svc.service.ts
+++ C-invoice/src/d-svc/d-svc.service.ts
@@ -1,8 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { CreateDSvcDto } from './common/dto/create-d-svc.dto';
-import { UpdateDSvcDto } from './common/dto/update-d-svc.dto';
 import { ClientProxy } from '@nestjs/microservices';
-import { UserResponse } from 'src/b-svc/common/type/user.response.type';
 
 @Injectable()
 export class DSvcService {
